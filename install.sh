dcf5a3c28e5b6488ef33ce18bc6cc172415c2394
Roman Rader <antigluk@gmail.com>
2010-01-20 18:58:48 +0000

    install.sh fixed

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "2) Adding enviromnent variable..."
 rm "/etc/profile.d/cprompt.sh"
 touch "/etc/profile.d/cprompt.sh"
 echo "CPROMPTINCLUDES is `pwd`/includes/"
-echo "export CPROMPTINCLUDES = `pwd`/includes/" > /etc/profile.d/cprompt.sh
+echo "export CPROMPTINCLUDES=`pwd`/includes/" > /etc/profile.d/cprompt.sh
 echo "Ok\n"
 echo "3) Creating symlink to CPrompt in /usr/bin..."
 rm /usr/bin/CPrompt
